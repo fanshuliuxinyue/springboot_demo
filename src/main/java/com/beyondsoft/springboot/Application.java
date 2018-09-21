818feaea15448355e6184146096bd0a217d96900
root <root@devopscentos>
2018-09-21 15:07:31 +0800

    Fix springboot packages scan problem

--- src/main/java/com/beyondsoft/springboot/Application.java
+++ src/main/java/com/beyondsoft/springboot/Application.java
@@ -8,7 +8,8 @@ import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ServletComponentScan
-@ComponentScan(basePackages={"com.beyondsoft.springboot.Interceptor"})
+@ComponentScan(basePackages={"com.beyondsoft.springboot.Interceptor",
+		"com.beyondsoft.springboot"})
 public class Application {
 
 	public static void main(String[] args) {
